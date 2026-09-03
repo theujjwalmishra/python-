46ac0a65f1c25ccc822af04b30ddff25d9c61cab
Ujjwal Mishra <ujjwalmishra.cse2024@agra.sharda.ac.in>
2026-09-03 10:31:49 +0530

    Updated Code

---
+++ chapter 5/2.while_loop.py
@@ -0,0 +1,9 @@
+# WAP to print the content of list using while loops.
+
+a = ["Rohan", "Ujjwal", "Mishra", 75]
+
+i = 0
+
+while(i<len(a)):
+    print(a[i])
+    i+=1
---
+++ chapter 5/3.for_loop.py
@@ -0,0 +1,17 @@
+# # Wap a table of 4 .
+
+# for i in range (4 , 41 , 4):
+#     print(i)
+
+
+# for i in range(0 , 101 , 4): # (starting , ending , difference)
+#     print(i)
+
+
+# we can use the for loops in all the things in string , in loops and in tuples also
+
+# string
+s = "ujjwal"
+
+for i in s:
+    print(i)
