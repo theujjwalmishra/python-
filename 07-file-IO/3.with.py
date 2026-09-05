7e18a5cab055d9270989fe605bd4e2eeaacad5c0
Ujjwal Mishra <ujjwalmishra.cse2024@agra.sharda.ac.in>
2026-09-05 22:48:07 +0530

    add

--- 07-file-IO/3.with.py
+++ 07-file-IO/3.with.py
@@ -1,1 +1,30 @@
-# best way to open and close 
+# # best way to open and close 
+
+# # read operations
+# with open("ujjwal.txt") as a:
+#     print(a.read())
+
+# # with variables
+# with open("ujjwal.txt") as a:
+#     b = a.read()
+#     print()
+
+# write operations
+with open("new.txt","w") as a:
+    a.write("Any body can do things")
+
+# other functions 
+
+# append operations
+with open("new.txt","a") as d:
+    d.write("\nokay bye ")
+
+# update 
+with open("ujjwal.txt","r+") as e:
+    e.write("Ujjwal is the champ . Yes there are bad times come but it is life \nYou have to fight there. You are champion bro .")
+
+# read
+
+with open("ujjwal.txt") as x:
+    z = x.read()
+    print(z)
